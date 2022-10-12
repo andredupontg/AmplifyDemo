f06158007b33643d56f3f9a3875d0930df7c2af9
André Dupont <andredupontg@hotmail.com>
2022-10-11 19:32:55 -0500

    ADD: Aws modules, libraries and front UI comp

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "demobackend": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
